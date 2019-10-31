b91fddf189271eb29e8ab7f40c24590ec7c0e24f
tinytacooo <39066347+tinytacooo@users.noreply.github.com>
2019-10-31 01:51:32 -0400

    Read data from individual pixels

--- read_bmp.asm
+++ read_bmp.asm
@@ -3,13 +3,15 @@
 	.data
 buf:		.space	2	# space in mem so main contents of .bmp file are word-aligned
 buffer:		.space	2000	# reserve space to store contents of .bmp file
-fin:		.asciiz	"test_8x8_16-color_lightgrey.bmp"
+ybr:		.space	2000	# reserve space to store YCbCr color space data
+fin:		.asciiz	"test_8x8_16-color_wb.bmp"
 fout:		.asciiz "testout.bmp"
 br:		.asciiz "\n"
 
 	.text
 main:
 	jal	read_bmp		# read bitmap image, get width and height
+	jal	convert_color_space	# convert RGB color space to YCbCr
 	#jal	print_pixel_data
 	j	exit
 
@@ -19,6 +21,11 @@ main:
 # $s2	bitmap image vertical height
 # $s3	bits per pixel - "1" indicates monochrome image <-- MIGHT JUST WANT TO ASSUME 16-BIT
 
+# ARGUMENTS: $a0 = name of file
+#            $a1 = compression ratio
+# RETURNS:   $s0 = address of start of image data in bitmap file
+#	     $s1 = bitmap image width
+#	     $s2 = bitmap image height
 read_bmp:
 	### open original bmp file ###
 	li	$v0, 13		# for file open
@@ -49,7 +56,39 @@ read_bmp:
 	lw	$s2, 8($t1)	# store height (8 bytes into the info header)
 	lh	$s3, 14($t1)	# store bits per pixel
 	
+	### testing jump ###
+	#addi $sp, $sp, -4
+	#sw $ra, 4($sp)
+	#jal test
+	#lw $ra, 4($sp)
+	#addi $sp, $sp, 4
+	
 	jr $ra			# return to main program
+
+# ARGUMENTS: 
+# RETURNS:   none
+convert_color_space:
+	addi	$sp, $sp, -4	# make space on the stack so we can call subroutines
+	sw	$ra, 4($sp)
+	
+	lhu	$a0, ($s0)	# loads 0x----xxxx data from word
+	jal	ccs_pixel
+	#lhu	$t1, 2($s0)	# loads 0xxxxx---- data from word
+	
+	lw	$ra, 4($sp)	# return stack to original state
+	addi	$sp, $sp, 4
+	jr $ra
+
+ccs_pixel:
+	andi	$t0, $a0, 15
+	andi	$t1, $a0, 240
+	srl	$t1, $t1, 4
+	andi	$t2, $a0, 3840
+	srl	$t2, $t2, 8
+	andi	$t3, $a0, 61440
+	srl	$t3, $t3, 12
+	jr	$ra
+	
 
 print_pixel_data:
 	subi	$t0, $s0, 4
